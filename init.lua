98ba4ec8d0b6e69b8e679c3d16c75873a5ba777c
poikilos <7557867+poikilos@users.noreply.github.com>
2020-02-21 15:18:23 -0500

    Add deep search. Add web config. Sanitize strings.

--- init.lua
+++ init.lua
@@ -25,24 +25,32 @@
 
 ]]--
 
-filter = { registered_on_violations = {} }
+
+filter = {registered_on_violations = {}}
+
+-- local configuration
 local words = {}
 local muted = {}
 local violations = {}
 local s = minetest.get_mod_storage()
 
-function filter.init()
-	local sw = s:get_string("words")
-	if sw and sw ~= "" then
-		words = minetest.parse_json(sw)
-	end
-
-	if #words == 0 then
-		filter.import_file(minetest.get_modpath("filter") .. "/words.txt")
-	end
+-- web configuration
+local filter_config = nil
+local filter_url = minetest.settings:get("filter_url") or "https://pastebin.com/raw/p5xkF0RW" -- "http://minetest.io/reputation/chat_filter.json"
+-- filter_url = "https://minetest.io/reputation/chat_filter.json"
+-- or "https://pastebin.com/raw/p5xkF0RW" but that may not work due to user agent allow list or other measures
+local filter_fuzzy = minetest.settings:get_bool("filter_fuzzy")
+if filter_fuzzy == nil then
+	filter_fuzzy = true
+end
+local filter_deep = minetest.settings:get_bool("filter_deep")
+if filter_deep == nil then
+	filter_deep = true
 end
 
-function filter.import_file(filepath)
+local http_api = minetest.request_http_api and minetest.request_http_api()  -- must occur in outer scope; requires trusted environment
+
+local function filter_import_file(filepath)
 	local file = io.open(filepath, "r")
 	if file then
 		for line in file:lines() do
@@ -57,28 +65,220 @@ function filter.import_file(filepath)
 	end
 end
 
-function filter.register_on_violation(func)
+local function filter_download(delay, player_name)
+	if http_api then
+		local function p_set_config(config)
+			if config.words and config.word_partials and config.deep_partials then
+				filter_config = config
+				local word_count = filter_config and filter_config.words and #filter_config.words
+				local deep_partials_count = filter_config and filter_config.deep_partials and #filter_config.deep_partials
+				local word_partials_count = filter_config and filter_config.word_partials and #filter_config.word_partials
+				local msg = "Processing downloaded filter completed. The current list has " .. word_count .. " word(s) and " .. deep_partials_count .. " deep partials and "..word_partials_count.." partial word(s)."
+				minetest.log("action", "[filter] " .. msg)
+				if player_name then
+					minetest.chat_send_player(player_name, msg)
+				end
+			else
+				local msg = " web config is incomplete and will not be used. It must contain words, word_partials, and deep_partials (empty lists are allowed)."
+				minetest.log("error", "[filter]" .. msg)
+				if player_name then
+					minetest.chat_send_player(player_name, msg)
+				end
+			end
+		end
+		local function p_set_config_json(config_json)
+			local config = minetest.parse_json(config_json)
+			p_set_config(config)
+		end
+		local req_handle = nil
+		-- local function read_handle()
+		-- 	if req_handle then
+		-- 		minetest.log("info", "[filter] reading request handle for " .. filter_url .. "...")
+		-- 		local result = http_api.fetch_async_get(req_handle)
+		-- 		minetest.log("info", "[filter] reading request handle for " .. filter_url .. "...checking result...")
+		-- 		fetch_callback(result)
+		-- 	else
+		-- 		minetest.log("error", "[filter] logic error " .. filter_url .. " not ready")
+		-- 	end
+		-- end
+		local function fetch_callback(result)
+			if not result then
+				minetest.log("info", "[filter] loading " .. filter_url .. "...NO RESULT")
+				return
+			else
+				if not result.completed then
+					minetest.log("info", "[filter] loading " .. filter_url .. "...")
+					return
+				end
+			end
+			if result.succeeded then
+				minetest.log("info", "[filter] loading " .. filter_url .. "...OK. Parsing...")
+				-- protected call (https://www.lua.org/pil/8.4.html):
+				if pcall(p_set_config, minetest.parse_json(result.data)) then
+					minetest.log("info", "[filter] Processing downloaded filter completed.")
+				else
+					local word_count = filter_config and filter_config.words and #filter_config.words
+					local deep_partials_count = filter_config and filter_config.deep_partials and #filter_config.deep_partials
+					local word_partials_count = filter_config and filter_config.word_partials and #filter_config.word_partials
+					local msg = "Processing downloaded filter failed. The current list has " .. word_count .. " word(s) and " .. deep_partials_count .. " deep partials and " .. word_partials_count .. " partial words."
+					if player_name then
+						minetest.chat_send_player(player_name, msg)
+					end
+					minetest.log("warning", "[filter] " .. msg)
+				end
+			else
+				if result.timeout then
+					minetest.log("info", "[filter] loading " .. filter_url .. "...TIMED OUT")
+				else
+					minetest.log("info", "[filter] loading " .. filter_url .. "...FAILED")
+				end
+			end
+		end
+		local function download_filter()
+			minetest.log("info", "[filter] fetching " .. filter_url .. "...")
+			http_api.fetch({url=filter_url, timeout=15}, fetch_callback)
+		end
+		-- local function download_filter_async()
+		-- 	req_handle = http_api.fetch_async({url=filter_url, timeout=15}, fetch_callback)
+		-- 	minetest.log("info", "[filter] reading" .. filter_url .. " after 10 seconds...")
+		-- 	minetest.after(10, read_handle)
+		-- end
+		-- if http_api.fetch_async_get then
+		-- 	minetest.log("info", "[filter] loading async " .. filter_url .. " after "..delay.." seconds...")
+		-- 	minetest.after(delay, download_filter_async)
+		-- else
+		minetest.log("info", "[filter] loading" .. filter_url .. " after "..delay.." seconds...")
+		minetest.after(delay, download_filter)
+		-- end
+	else
+		local msg = "http_api is not available (you must add filter to secure.trusted_mods to use this feature of filter)"
+		if player_name then
+			minetest.chat_send_player(player_name, msg)
+		end
+		minetest.log("warning", "[filter] " .. msg)
+	end
+end
+
+local function filter_init()
+	local sw = s:get_string("words")
+	if sw and sw ~= "" then
+		words = minetest.parse_json(sw)
+	end
+
+	if #words == 0 then
+		filter_import_file(minetest.get_modpath("filter") .. "/words.txt")
+	end
+	filter_download(10, nil)
+end
+
+local function filter_register_on_violation(func)
 	table.insert(filter.registered_on_violations, func)
 end
 
-function filter.check_message(name, message)
-	for _, w in ipairs(words) do
-		if string.find(message:lower(), "%f[%a]" .. w .. "%f[%A]") then
+local function filter_speakable(raw_message)
+	local sanitized = raw_message
+	-- local not_language = "\t\n\r"
+	-- for c in string.gmatch(not_language, ".") do
+	--	sanitized = sanitized:gsub(c, "")
+	-- end
+	-- NOTE: sanitized:gsub("[%c%p%s]", "") removes all special & space
+	-- %c is control characters (includes newlines, tab, etc.)
+	-- %p is punctuation [prevent rule evasion with dots/hyphens/etc]
+	-- %s is all whitespace
+	-- str:gsub( "%W", "" ) removes all non-word characters
+	return sanitized:gsub("[%c%p]", "")
+end
+
+local filter_debug_next = true
+
+local function filter_check_message(name, message)
+	local message_lower = message:lower()
+	for _, needle in ipairs(words) do
+		if string.find(message_lower, "%f[%a]" .. needle .. "%f[%A]") then
 			return false
 		end
 	end
-
+	local more = filter_config and filter_config.words
+	local sanitized = filter_speakable(message_lower)
+	if more then
+		for _, needle in ipairs(more) do
+			if string.find(sanitized, "%f[%a]" .. needle .. "%f[%A]") then
+				return false
+			end
+			if filter_debug_next then
+				minetest.log("verbose", "[filter] " .. needle .. " is not a WORD in '" .. sanitized .. "'")
+				-- is_debug_warning_shown = true
+			end
+		end
+	else
+		if filter_debug_next then
+			minetest.log("warning", "[filter] has no filter_config.words")
+			is_debug_warning_shown = true
+		end
+	end
+	local word_partials = filter_config and filter_config.word_partials
+	if word_partials then
+		for _, needle in ipairs(word_partials) do
+			for match in string.gmatch(sanitized, "[^%s]+") do
+				for partial_match in string.gmatch(match, needle) do
+					return false
+				end
+			end
+			if filter_debug_next then
+				minetest.log("verbose", "[filter] " .. needle .. " is not a WORD in '" .. sanitized .. "'")
+				-- is_debug_warning_shown = true
+			end
+		end
+	else
+		if filter_debug_next then
+			minetest.log("warning", "[filter] has no filter_config.words")
+			is_debug_warning_shown = true
+		end
+	end
+	local deep_partials = filter_config and filter_config.deep_partials
+	local is_debug_warning_shown = false
+	if filter_deep then
+		if deep_partials then
+			-- local no_spaces = sanitized:gsub("[%c%p%s]", "")
+			local no_spaces = sanitized:gsub("[%W]", "")
+			for _, needle in ipairs(deep_partials) do
+				for match in string.gmatch(no_spaces, needle) do
+					return false
+				end
+				if filter_debug_next then
+					minetest.log("verbose", "[filter] " .. needle .. " is not in '" .. no_spaces .. "'")
+					-- is_debug_warning_shown = true
+				end
+			end
+		else
+			if filter_debug_next then
+				minetest.log("warning", "[filter] has no filter_config.deep_partials")
+				is_debug_warning_shown = true
+			end
+		end
+	else
+		if filter_debug_next then
+			minetest.log("warning", "[filter] filter_deep is off")
+			is_debug_warning_shown = true
+		end
+	end
+	if filter_fuzzy then
+		-- TODO: not yet implemented
+	end
+	if is_debug_warning_shown then
+		filter_debug_next = false
+	end
 	return true
 end
 
-function filter.mute(name, duration)
+local function filter_mute(name, duration)
 	do
 		local privs = minetest.get_player_privs(name)
 		privs.shout = nil
 		minetest.set_player_privs(name, privs)
 	end
 
-	minetest.chat_send_player(name, "Watch your language! You have been temporarily muted")
+	minetest.chat_send_player(name, "You have been temporarily muted for abusing the chat.")
 
 	muted[name] = true
 
@@ -96,7 +296,7 @@ function filter.mute(name, duration)
 	end)
 end
 
-function filter.show_warning_formspec(name)
+local function filter_show_warning_formspec(name)
 	local formspec = "size[7,3]bgcolor[#080808BB;true]" .. default.gui_bg .. default.gui_bg_img .. [[
 		image[0,0;2,2;filter_warning.png]
 		label[2.3,0.5;Please watch your language!]
@@ -115,7 +315,7 @@ function filter.show_warning_formspec(name)
 	minetest.show_formspec(name, "filter:warning", formspec)
 end
 
-function filter.on_violation(name, message)
+local function filter_on_violation(name, message)
 	violations[name] = (violations[name] or 0) + 1
 
 	local resolution
@@ -129,18 +329,18 @@ function filter.on_violation(name, message)
 	if not resolution then
 		if violations[name] == 1 and minetest.get_player_by_name(name) then
 			resolution = "warned"
-			filter.show_warning_formspec(name)
+			filter_show_warning_formspec(name)
 		elseif violations[name] <= 3 then
 			resolution = "muted"
-			filter.mute(name, 1)
+			filter_mute(name, 1)
 		else
 			resolution = "kicked"
-			minetest.kick_player(name, "Please mind your language!")
+			minetest.kick_player(name, "You have been kicked for abusing the chat.")
 		end
 	end
 
 	local logmsg = "VIOLATION (" .. resolution .. "): <" .. name .. "> "..  message
-	minetest.log("action", logmsg)
+	minetest.log("action", "[filter] " .. logmsg)
 
 	local email_to = minetest.settings:get("filter.email_to")
 	if email_to and minetest.global_exists("email") then
@@ -159,8 +359,8 @@ table.insert(minetest.registered_on_chat_messages, 1, function(name, message)
 		return true
 	end
 
-	if not filter.check_message(name, message) then
-		filter.on_violation(name, message)
+	if not filter_check_message(name, message) then
+		filter_on_violation(name, message)
 		return true
 	end
 end)
@@ -168,8 +368,8 @@ end)
 
 local function make_checker(old_func)
 	return function(name, param)
-		if not filter.check_message(name, param) then
-			filter.on_violation(name, param)
+		if not filter_check_message(name, param) then
+			filter_on_violation(name, param)
 			return false
 		end
 
@@ -211,13 +411,16 @@ minetest.register_chatcommand("filter", {
 		local cmd, val = param:match("(%w+) (.+)")
 		if param == "list" then
 			return true, #words .. " words: " .. table.concat(words, ", ")
+		elseif param == "download" then
+			filter_download(0, name)
+			return true, "Checking " .. filter_url .. "."
 		elseif cmd == "add" then
 			table.insert(words, val)
 			s:set_string("words", minetest.write_json(words))
 			return true, "Added \"" .. val .. "\"."
 		elseif cmd == "remove" then
-			for i, w in ipairs(words) do
-				if w == val then
+			for i, needle in ipairs(words) do
+				if needle == val then
 					table.remove(words, i)
 					s:set_string("words", minetest.write_json(words))
 					return true, "Removed \"" .. val .. "\"."
@@ -246,4 +449,4 @@ minetest.register_on_shutdown(function()
 	end
 end)
 
-filter.init()
+filter_init()
--- readme.md
+++ readme.md
@@ -1,38 +1,75 @@
-# filter mod
+# Minetest Mod: Advanced Filter [filter]
 
-This mod adds a simple chat filter. There is no default word list,
-and adding words to the filter list is done through the `/filter`
-chat command. You need the `server` priv to use the chat command.
+Filter language using an advanced system of filters including pre- and
+post-sanitized word and partial word searches.
+
+There is a default configuration at the URL specified by the default
+`filter_url`. The default configuration includes words, word_partials,
+and deep_partials (checked after removing spaces from the message).
+
+There is no default local word list, and adding words to the local word
+list is done through the `/filter` chat command. You need the `server`
+priv to use the chat command. Removing words in this way does not affect
+the web word list.
 
 The `/filter` chat command can `add`, `remove` or `list` words. The
 words are stored in `mod_storage`, which means that this mod requires
-0.4.16 or above to function.
+0.4.16 or above to function. Type `/filter download` to re-download the
+web configuration from the URL specified as `filter_url`.
 
 If a player speaks a word that is listed in the filter list, they are
 muted for 1 minute. After that, their `shout` privilege is restored.
 If they leave, their `shout` privilege is still restored, but only after
 the time expires, not before.
 
-## API
 
-### Callbacks
+## Differences from minetest-mods/filter
+- Detect and ignore non-spoken characters.
+- `filter_deep = true`: check for partial words after removing spaces to
+  reduce filter evasion.
+- Download filter settings from the web (that part is optional, and is
+  turned on by setting this as a trusted mod).
+  - Set a custom URL in minetest.conf: `filter_url = ` (see
+    settingtypes.txt for default)
+  - Add `secure.http_mods = filter` (or `secure.trusted_mods = filter`)
+    along with any other mods you may need to have that permission.
+    (otherwise, `minetest.request_http_api()` returns nil when
+    `secure.enable_security = true` [which is the default if not set in
+    minetest.conf])
+  - The URL must provide json in the following format ("_comment" is
+    optional):
+```json
+{
+"_comment": "This page is only intended to be read by the strong language filter program, not displayed.",
+"partials": [],
+"word_partials": [],
+"words": []
+}
+```
+  - `words` are compared to words in each chat message.
+  - The filter looks for `word_partials` inside words of the message.
+  - `partials` is the most strict, since spaces are removed from the
+    message before the check.
+  - [ ] Adding partials to the deep partials list while filter_fuzzy
+    is on will reduce performance slightly, since every character of
+    the message will be checked to see if it starts with a word similar
+    to the string (see https://github.com/profan/lua-bk-tree).
+- See settingtypes.txt for additional explanations of the options.
 
-* filter.register_on_violation(func(name, message, violations))
-	* Violations is the value of the player's violation counter - which is
-	  incremented on a violation, and halved every 10 minutes.
-	* Return true if you've handled the violation. No more callbacks will be
-	  executation, and the default behaviour (warning/mute/kick) on violation
-	  will be skipped.
 
-### Methods
+## Download
+- [Download ZIP](https://github.com/poikilos/filter/archive/master.zip)
+- `git clone https://github.com/poikilos/filter.git`
+- [Browse Source on GitHub](https://github.com/poikilos/filter)
 
-* filter.import_file(path)
-	* Input bad words from a file (`path`) where each line is a new word.
-* filter.check_message(name, message)
-	* Checks message for violation. Returns true if okay, false if bad.
-	  If it returns false, you should cancel the sending of the message and
-	  call filter.on_violation()
-* filter.on_violation(name, message)
-	* Increments violation count, runs callbacks, and punishes the players.
-* filter.mute(name, duration)
-* filter.show_warning_formspec(name)
+
+## Developer Notes
+(Poikilos)
+
+### Tasks
+- [x] Implement web-based configuration.
+- [x] Implement deep search (without spaces)
+  - [x] Remove partials and split into deep_partials and word_partials.
+    - [ ] Remove partials from the copy of the config on pastebin.
+- [x] Remove public functions.
+- [ ] Implement `filter_fuzzy`.
